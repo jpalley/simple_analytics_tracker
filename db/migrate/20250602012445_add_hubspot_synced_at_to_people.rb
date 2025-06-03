9237112ff80bff05e44095ff9ee8c3def06f1516
Jonathan Palley <jpalley@gmail.com>
2025-06-02 18:21:01 -0700

    something odd with hubspot synced at

--- db/migrate/20250602012445_add_hubspot_synced_at_to_people.rb
+++ db/migrate/20250602012445_add_hubspot_synced_at_to_people.rb
@@ -1,6 +1,6 @@
 class AddHubspotSyncedAtToPeople < ActiveRecord::Migration[8.0]
   def change
-    add_column :people, :hubspot_synced_at, :datetime
+    # add_column :people, :hubspot_synced_at, :datetime
     add_index :people, :hubspot_synced_at
   end
 end
